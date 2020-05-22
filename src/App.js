1c11d029bc6981b4b2fbb705353f6eceddc0c3be
Behnam Mahmoodi <behnam.itc@outlook.com>
2020-05-22 18:09:43 +0430

    Finish Project

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends React.Component {
     );
     return (
       <div className="App">
+        <h1>لیست ربات های فارسی</h1>
         <SearchBox
           placeholder="جستجو هیولا..."
           handleChange={this.handleChange}
